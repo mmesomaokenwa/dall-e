fb7873e02abe30a5a67911a5ae5695ce41b1eac2
Mmesoma Okenwa <godsfavourokenwa12@gmail.com>
2024-03-17 12:21:51 +0100

    third commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,15 @@
 export const createPost = async (post) => {
   try {
-    const response = await fetch('http://localhost:8080/api/v1/post', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
+    const response = await fetch(
+      "https://dall-e-tjyx.onrender.com/api/v1/post",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(post),
+      }
+    );
     if (!response.ok) throw new Error('An error occurred while creating a post')
     const data = await response.json()
     return data
@@ -17,7 +20,9 @@ export const createPost = async (post) => {
 
 export const getPosts = async () => {
   try {
-    const response = await fetch('http://localhost:8080/api/v1/post')
+    const response = await fetch(
+      "https://dall-e-tjyx.onrender.com/api/v1/post"
+    );
     if (!response.ok) throw new Error('An error occurred while fetching posts')
     const data = await response.json()
     return data.data.reverse()
@@ -28,15 +33,18 @@ export const getPosts = async () => {
 
 export const generateImage = async (prompt) => {
   try {
-    const response = await fetch('http://localhost:8080/api/v1/dalle', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        prompt
-      })
-    })
+    const response = await fetch(
+      "https://dall-e-tjyx.onrender.com/api/v1/dalle",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      }
+    );
     if (!response.ok) throw new Error('An error occurred while generating an image')
     const data = await response.json()
     return data
